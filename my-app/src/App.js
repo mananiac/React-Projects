a12679cd25a0002d8d6d6d89242c136fa2290259
MFA21SCM41S <manan.sagar@yahoo.com>
2023-01-21 19:21:27 -0600

    Making routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,8 @@ import TextForm from "./components/TextForm";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   const [mode, newMode] = useState("light");
   const [alert, setAlert] = useState(null);
@@ -32,23 +34,33 @@ function App() {
   };
   return (
     <>
-      <Navbar
-        title="Text Utilities"
-        aboutText="About"
-        mode={mode}
-        toggleMode={toggleMode}
-      />
       <Alert alert={alert} />
+      
+      
+      <Router>
+      <Navbar
+            title="Text Utilities"
+            aboutText="About"
+            mode={mode}
+            toggleMode={toggleMode}
+          />
+          <Routes>
+            <Route path="/about" element={<About />} />
 
-      <div className="container my-3">
-        <TextForm
-          showAlert={showAlert}
-          heading="Enter text to analyse below"
-          mode={mode}
-        />
-
-        <About />
-      </div>
+            <Route
+              path="/"
+              element={
+                <div className="container">
+                <TextForm
+                  heading="Enter text to analyze"
+                  mode={mode}
+                  showAlert={showAlert}
+                /></div>
+              }
+            />
+          </Routes>
+        </Router>
+      
     </>
   );
 }
